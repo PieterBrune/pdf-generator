ec55b91e1749a54619502baa12546b36c9f2e579
pieter brune <pebrune@gmail.com>
2022-05-23 18:43:38 +0800

    Created Invoices controller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,7 +1,29 @@
 class InvoicesController < ApplicationController
   def index
+    @invoices = Invoice.all
   end
 
   def show
+    @invoice = Invoice.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "Invoice No. #{@invoice.id}",
+               page_size: 'A4',
+               template: "invoices/show.html.erb",
+               layout: "pdf.html",
+               orientation: "Landscape",
+               lowquality: true,
+               zoom: 1,
+               dpi: 75
+      end
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:to_full_name, :to_street_address, :to_city, :to_state, :to_postal_code, :to_country, :to_phone_number, :status, :discount, :vat)
   end
 end
